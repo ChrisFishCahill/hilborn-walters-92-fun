cabc28706ff47833ac80ff01b5d0d1f33f9ba750
Christopher Cahill <you@example.com>
2021-07-27 10:47:13 -0600

    add in natural surival rate calculation

--- rec-reconstruction/assignment.R
+++ rec-reconstruction/assignment.R
@@ -8,7 +8,11 @@ str(data)
 #Catch (millions), Effort (million hooks)
 #----------------------------------------------------
 
-# S annual finite survival--
+# S annual finite survival rate
+#S = e^(-M), where M is instantaneous natural mortality
 # What does fishbase say? e.g., 
 # http://www.fishbase.org/Summary/SpeciesSummary.php?ID=143&AT=yellowfin+tuna
-#
+# See also Hampton 1992, M ranges from 0.3 to ~ 2.0?! 
+
+S <- exp(-0.6)
+
