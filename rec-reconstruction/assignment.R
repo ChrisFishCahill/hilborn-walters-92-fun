b8767983941b6a5a00d000b21bd2b393c7298ae8
Christopher Cahill <you@example.com>
2021-07-27 10:52:00 -0600

    back calculate Nt all years

--- rec-reconstruction/assignment.R
+++ rec-reconstruction/assignment.R
@@ -14,5 +14,16 @@ str(data)
 # http://www.fishbase.org/Summary/SpeciesSummary.php?ID=143&AT=yellowfin+tuna
 # See also Hampton 1992, M ranges from 0.3 to ~ 2.0?! 
 
+#Set up leading parameters
 S <- exp(-0.6)
+q <- 0.01145 #estimated from assessment models
+
+#General model: 
+#N[t+1] = S(N[t]-C[t]) + R[t]
+#Solve for R[t]: 
+#R[t] = N[t+1] - S(N[t] - C[t])
+
+#Back calculate Nt for all years
+data$N <- data$mean_cpue / q
+
 
