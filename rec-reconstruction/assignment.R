339464fc48d0e84c0524668a200b1a0408865d9a
Christopher Cahill <you@example.com>
2021-07-27 11:17:47 -0600

    calculate the Rpreds

--- rec-reconstruction/assignment.R
+++ rec-reconstruction/assignment.R
@@ -9,15 +9,6 @@ str(data)
 #CPUE from Japan longlines
 #Catch (millions), Effort (million hooks)
 #----------------------------------------------------
-
-# S annual finite survival rate
-#S = e^(-M), where M is instantaneous natural mortality
-# What does fishbase say? e.g., 
-# http://www.fishbase.org/Summary/SpeciesSummary.php?ID=143&AT=yellowfin+tuna
-# See also Hampton 1992, M ranges from 0.3 to ~ 2.0?! 
-
-#Set up leading parameters
-S <- exp(-0.6)
 q <- 0.01145 #estimated from assessment models
 
 #Back calculate Nt for all years
@@ -52,4 +43,23 @@ ggsave("rec-reconstruction/N_vs_catch.pdf",
 #N[t+1] = S(N[t]-C[t]) + R[t]
 #Solve for R[t]: 
 #R[t] = N[t+1] - S(N[t] - C[t])
+
+# S annual finite survival rate
+#S = e^(-M), where M is instantaneous natural mortality
+# What does fishbase say? e.g., 
+# http://www.fishbase.org/Summary/SpeciesSummary.php?ID=143&AT=yellowfin+tuna
+# See also Hampton 1992, M ranges from 0.3 to ~ 2.0?! 
+
+#Set up leading parameters
+S <- exp(-0.6)
+n_years <- length(unique(data$year))
+
+#Set up a vector to put recruitment predictions into
+data$Rpred <- NA
+
+#Do the recursion
+for(t in (n_years-1):1){ 
+  data$Rpred[t] <- data$N[t+1] - S*(data$N[t] - data$catch[t])
+}
+
 
