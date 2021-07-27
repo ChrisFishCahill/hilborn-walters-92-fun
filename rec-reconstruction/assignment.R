b5dfb342cb519ca05f80aa268519f402a685ec80
Christopher Cahill <you@example.com>
2021-07-27 10:45:19 -0600

    bump hampton, S calculation

--- rec-reconstruction/assignment.R
+++ rec-reconstruction/assignment.R
@@ -4,6 +4,11 @@ str(data)
 #----------------------------------------------------
 #Notes on data
 #Pacific Yellowfin Tuna, total catches over gear types
-#CPUE from Japan lonlines
+#CPUE from Japan longlines
 #Catch (millions), Effort (million hooks)
 #----------------------------------------------------
+
+# S annual finite survival--
+# What does fishbase say? e.g., 
+# http://www.fishbase.org/Summary/SpeciesSummary.php?ID=143&AT=yellowfin+tuna
+#
