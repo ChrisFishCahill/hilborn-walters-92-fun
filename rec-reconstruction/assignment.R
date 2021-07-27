ec06f5f69da3471dd7a3094c15a328959dd3c5b0
Christopher Cahill <you@example.com>
2021-07-27 10:27:25 -0600

    Read in data, add notes on data str

---
+++ rec-reconstruction/assignment.R
@@ -0,0 +1,9 @@
+data <- read.csv("rec-reconstruction/data.csv")
+str(data)
+
+#----------------------------------------------------
+#Notes on data
+#Pacific Yellowfin Tuna, total catches over gear types
+#CPUE from Japan lonlines
+#Catch (millions), Effort (million hooks)
+#----------------------------------------------------
